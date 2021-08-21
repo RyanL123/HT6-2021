f1b8da19f75df42452b44f2731897a37f37f5ac4
Ryan Li <ryan.lichangrui@gmail.com>
2021-08-20 21:57:13 -0400

    firebase authentication

---
+++ src/components/Nav.js
@@ -0,0 +1,157 @@
+import React from "react"
+import {
+    chakra,
+    Avatar,
+    Box,
+    Flex,
+    useColorModeValue,
+    VisuallyHidden,
+    HStack,
+    Button,
+    useDisclosure,
+    VStack,
+    IconButton,
+    CloseButton,
+} from "@chakra-ui/react"
+import { AiOutlineMenu } from "react-icons/ai"
+import firebase from "../firebase"
+
+const Nav = () => {
+    const bg = useColorModeValue("white", "gray.800")
+    const mobileNav = useDisclosure()
+    function signIn() {
+        var provider = new firebase.auth.GoogleAuthProvider()
+        firebase
+            .auth()
+            .signInWithPopup(provider)
+            .then(result => {
+                /** @type {firebase.auth.OAuthCredential} */
+                var credential = result.credential
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                var token = credential.accessToken
+                // The signed-in user info.
+                var user = result.user
+                localStorage.setItem("user", JSON.stringify(user))
+            })
+            .catch(error => {
+                // Handle Errors here.
+                var errorCode = error.code
+                var errorMessage = error.message
+                // The email of the user's account used.
+                var email = error.email
+                // The firebase.auth.AuthCredential type that was used.
+                var credential = error.credential
+                // ...
+            })
+    }
+    return (
+        <React.Fragment>
+            <chakra.header
+                bg={bg}
+                w="full"
+                px={{ base: 2, sm: 4 }}
+                py={4}
+                shadow="md"
+            >
+                <Flex
+                    alignItems="center"
+                    justifyContent="space-between"
+                    mx="auto"
+                >
+                    <Flex>
+                        <chakra.a
+                            href="/"
+                            title="Choc Home Page"
+                            display="flex"
+                            alignItems="center"
+                        >
+                            <VisuallyHidden>Choc</VisuallyHidden>
+                        </chakra.a>
+                        <chakra.h1 fontSize="xl" fontWeight="medium" ml="2">
+                            Choc
+                        </chakra.h1>
+                    </Flex>
+                    <HStack display="flex" alignItems="center" spacing={1}>
+                        <HStack
+                            spacing={1}
+                            mr={1}
+                            color="brand.500"
+                            display={{ base: "none", md: "inline-flex" }}
+                        >
+                            <Button variant="ghost">Features</Button>
+                            <Button variant="ghost">Pricing</Button>
+                            <Button variant="ghost">Blog</Button>
+                            <Button variant="ghost">Company</Button>
+                            {localStorage.getItem("user") ? (
+                                <Avatar
+                                    name=""
+                                    src={
+                                        JSON.parse(localStorage.getItem("user"))
+                                            .photoURL
+                                    }
+                                />
+                            ) : (
+                                <Button
+                                    variant="ghost"
+                                    onClick={() => signIn()}
+                                >
+                                    Sign in
+                                </Button>
+                            )}
+                        </HStack>
+                        <Box display={{ base: "inline-flex", md: "none" }}>
+                            <IconButton
+                                display={{ base: "flex", md: "none" }}
+                                aria-label="Open menu"
+                                fontSize="20px"
+                                color={useColorModeValue("gray.800", "inherit")}
+                                variant="ghost"
+                                icon={<AiOutlineMenu />}
+                                onClick={mobileNav.onOpen}
+                            />
+
+                            <VStack
+                                pos="absolute"
+                                top={0}
+                                left={0}
+                                right={0}
+                                display={mobileNav.isOpen ? "flex" : "none"}
+                                flexDirection="column"
+                                p={2}
+                                pb={4}
+                                m={2}
+                                bg={bg}
+                                spacing={3}
+                                rounded="sm"
+                                shadow="sm"
+                            >
+                                <CloseButton
+                                    aria-label="Close menu"
+                                    onClick={mobileNav.onClose}
+                                />
+
+                                <Button w="full" variant="ghost">
+                                    Features
+                                </Button>
+                                <Button w="full" variant="ghost">
+                                    Pricing
+                                </Button>
+                                <Button w="full" variant="ghost">
+                                    Blog
+                                </Button>
+                                <Button w="full" variant="ghost">
+                                    Company
+                                </Button>
+                                <Button w="full" variant="ghost">
+                                    Sign in
+                                </Button>
+                            </VStack>
+                        </Box>
+                    </HStack>
+                </Flex>
+            </chakra.header>
+        </React.Fragment>
+    )
+}
+
+export default Nav
---
+++ src/firebase.js
@@ -0,0 +1,25 @@
+import firebase from "firebase/app"
+import "firebase/auth"
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDeiL1T0jGV_wD7eGHckry3DCPcVsKfJzI",
+    authDomain: "ht6-2021.firebaseapp.com",
+    projectId: "ht6-2021",
+    storageBucket: "ht6-2021.appspot.com",
+    messagingSenderId: "406993166422",
+    appId: "1:406993166422:web:4122ccc155e9a1e5d51bf7",
+}
+firebase.initializeApp(firebaseConfig)
+
+firebase
+    .auth()
+    .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
+    .then(() => {})
+    .catch(error => {
+        // Handle Errors here.
+        var errorCode = error.code
+        var errorMessage = error.message
+    })
+
+export default firebase
