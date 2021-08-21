e4490298c3c9d2a75adab1534da3a0264f01fc69
unknown <jeffreyzhao2003@gmail.com>
2021-08-21 18:58:21 -0400

    added completed recipe button and changed layout

--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import {
     chakra,
     Box,
+    Button,
     Image,
     Flex,
     Text, 
@@ -9,10 +10,19 @@ import {
     StackDivider,
     Checkbox,
     Heading,
-    Center
+    Center,
+    NumberInput,
+    NumberInputField,
+    NumberInputStepper,
+    NumberIncrementStepper,
+    NumberDecrementStepper,
 } from "@chakra-ui/react";
 
+import {Link } from "react-router-dom";
+
 const Recipe = () =>{
+    const [ servings, setServings ] = useState(1)
+
     return (
         <Center
             w="100%"
@@ -25,8 +35,11 @@ const Recipe = () =>{
             >
                 <Flex 
                     direction="column"
+                    
                 >
-                    <Heading>
+                    <Heading
+                    textAlign={["center", null, "left"]}
+                    >
                         Food Name
                     </Heading>
                     <Image 
@@ -34,26 +47,42 @@ const Recipe = () =>{
                         fallbackSrc="https://via.placeholder.com/150"
                         boxSize="50vh"
                         borderRadius="5%"
+                        alignSelf="center"
                     />
-                    <Text fontSize="2xl">
-                        Ingredients
-                    </Text>
-                    <Stack spacing={4} direction="column">
-                        <Checkbox colorScheme="green" size="lg" defaultIsChecked>
-                            Item 1
-                        </Checkbox>
-                        <Checkbox colorScheme="green" size="lg" defaultIsChecked>
-                            Item 2
-                        </Checkbox>
-                        <Checkbox colorScheme="green" size="lg" defaultIsChecked>
-                            Item 3
-                        </Checkbox>
-                        <Checkbox colorScheme="green" size="lg" defaultIsChecked>
-                            Item 4
-                        </Checkbox>
-                    </Stack>
+                    <Box mt="2rem">
+                        <Text fontSize="2xl">
+                            Ingredients                         
+                        </Text>
+                        <Flex direction="row" my="1rem">
+                            <Text fontSize="xl" mr="1rem">
+                                Servings                      
+                            </Text>                          
+                            <NumberInput defaultValue={servings} min={0} w="20%">
+                                <NumberInputField />
+                                <NumberInputStepper>
+                                    <NumberIncrementStepper />
+                                    <NumberDecrementStepper />
+                                </NumberInputStepper>
+                            </NumberInput>
+                        </Flex>
+
+                        <Stack spacing={4} direction="column">
+                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                                Item 1
+                            </Checkbox>
+                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                                Item 2
+                            </Checkbox>
+                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                                Item 3
+                            </Checkbox>
+                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                                Item 4
+                            </Checkbox>
+                        </Stack>
+                    </Box>
                 </Flex>
-                <Box w={["90%", null, "50%"]}>
+                <Box w={["90%", null, "50%"]} mt={["2rem", null, "0"]}>
                     <Text fontSize="2xl">
                         Instructions
                     </Text>
@@ -83,6 +112,11 @@ const Recipe = () =>{
                             Instructions for step 4
                         </Text>
                     </Stack>
+                    <Button mt="2rem">
+                    <Link to="/dashboard">
+                        I made this recipe!
+                    </Link>
+                    </Button>
                 </Box>
             </Flex>
         </Center>
