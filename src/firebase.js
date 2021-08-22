89df802231000401d81d93f8069dff6525a44b43
Ryan Li <ryan.lichangrui@gmail.com>
2021-08-21 23:14:22 -0400

    format all files with prettier

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
-import firebase from "firebase/app"
-import "firebase/auth"
-import "firebase/firestore"
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: "AIzaSyDeiL1T0jGV_wD7eGHckry3DCPcVsKfJzI",
@@ -9,17 +9,17 @@ const firebaseConfig = {
     storageBucket: "ht6-2021.appspot.com",
     messagingSenderId: "406993166422",
     appId: "1:406993166422:web:4122ccc155e9a1e5d51bf7",
-}
-firebase.initializeApp(firebaseConfig)
+};
+firebase.initializeApp(firebaseConfig);
 
 firebase
     .auth()
     .setPersistence(firebase.auth.Auth.Persistence.LOCAL)
     .then(() => {})
-    .catch(error => {
+    .catch((error) => {
         // Handle Errors here.
-        var errorCode = error.code
-        var errorMessage = error.message
-    })
+        var errorCode = error.code;
+        var errorMessage = error.message;
+    });
 
-export default firebase
+export default firebase;
--- src/pages/Recipe.js
+++ src/pages/Recipe.js
@@ -5,7 +5,7 @@ import {
     Button,
     Image,
     Flex,
-    Text, 
+    Text,
     Stack,
     StackDivider,
     Checkbox,
@@ -18,46 +18,41 @@ import {
     NumberDecrementStepper,
 } from "@chakra-ui/react";
 
-import {Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Recipe = () =>{
-    const [ servings, setServings ] = useState(1)
+const Recipe = () => {
+    const [servings, setServings] = useState(1);
 
     return (
-        <Center
-            w="100%"
-        >
+        <Center w="100%">
             <Flex
                 w="80%"
                 direction={["column", null, "row"]}
                 justifyContent="space-around"
                 my="2rem"
             >
-                <Flex 
-                    direction="column"
-                    
-                >
-                    <Heading
-                    textAlign={["center", null, "left"]}
-                    >
+                <Flex direction="column">
+                    <Heading textAlign={["center", null, "left"]}>
                         Food Name
                     </Heading>
-                    <Image 
-                        src="https://thewoksoflife.com/wp-content/uploads/2016/02/beef-noodle-soup-7-1.jpg" 
+                    <Image
+                        src="https://thewoksoflife.com/wp-content/uploads/2016/02/beef-noodle-soup-7-1.jpg"
                         fallbackSrc="https://via.placeholder.com/150"
                         boxSize="50vh"
                         borderRadius="5%"
                         alignSelf="center"
                     />
                     <Box mt="2rem">
-                        <Text fontSize="2xl">
-                            Ingredients                         
-                        </Text>
+                        <Text fontSize="2xl">Ingredients</Text>
                         <Flex direction="row" my="1rem">
                             <Text fontSize="xl" mr="1rem">
-                                Servings                      
-                            </Text>                          
-                            <NumberInput defaultValue={servings} min={0} w="20%">
+                                Servings
+                            </Text>
+                            <NumberInput
+                                defaultValue={servings}
+                                min={0}
+                                w="20%"
+                            >
                                 <NumberInputField />
                                 <NumberInputStepper>
                                     <NumberIncrementStepper />
@@ -67,60 +62,68 @@ const Recipe = () =>{
                         </Flex>
 
                         <Stack spacing={4} direction="column">
-                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                            <Checkbox
+                                colorScheme="green"
+                                size="lg"
+                                defaultIsChecked
+                            >
                                 Item 1
                             </Checkbox>
-                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                            <Checkbox
+                                colorScheme="green"
+                                size="lg"
+                                defaultIsChecked
+                            >
                                 Item 2
                             </Checkbox>
-                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                            <Checkbox
+                                colorScheme="green"
+                                size="lg"
+                                defaultIsChecked
+                            >
                                 Item 3
                             </Checkbox>
-                            <Checkbox colorScheme="green" size="lg" defaultIsChecked>
+                            <Checkbox
+                                colorScheme="green"
+                                size="lg"
+                                defaultIsChecked
+                            >
                                 Item 4
                             </Checkbox>
                         </Stack>
                     </Box>
                 </Flex>
                 <Box w={["90%", null, "50%"]} mt={["2rem", null, "0"]}>
-                    <Text fontSize="2xl">
-                        Instructions
-                    </Text>
-                    <Stack spacing={4} direction="column" divider={<StackDivider borderColor="gray.200" />}>
+                    <Text fontSize="2xl">Instructions</Text>
+                    <Stack
+                        spacing={4}
+                        direction="column"
+                        divider={<StackDivider borderColor="gray.200" />}
+                    >
                         <Checkbox colorScheme="green" size="lg">
                             Step 1
                         </Checkbox>
-                        <Text fontSize="lg">
-                            Instructions for step 1
-                        </Text>
+                        <Text fontSize="lg">Instructions for step 1</Text>
                         <Checkbox colorScheme="green" size="lg">
                             Step 2
                         </Checkbox>
-                        <Text fontSize="lg">
-                            Instructions for step 2
-                        </Text>
+                        <Text fontSize="lg">Instructions for step 2</Text>
                         <Checkbox colorScheme="green" size="lg">
                             Step 3
                         </Checkbox>
-                        <Text fontSize="lg">
-                            Instructions for step 3
-                        </Text>
+                        <Text fontSize="lg">Instructions for step 3</Text>
                         <Checkbox colorScheme="green" size="lg">
                             Step 4
                         </Checkbox>
-                        <Text fontSize="lg">
-                            Instructions for step 4
-                        </Text>
+                        <Text fontSize="lg">Instructions for step 4</Text>
                     </Stack>
                     <Button mt="2rem">
-                    <Link to="/dashboard">
-                        I made this recipe!
-                    </Link>
+                        <Link to="/dashboard">I made this recipe!</Link>
                     </Button>
                 </Box>
             </Flex>
         </Center>
-    )
-}
+    );
+};
 
-export default Recipe
+export default Recipe;
