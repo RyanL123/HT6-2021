078fa8b7c36a4183900e4a8227ee62e85437132b
Ryan Li <ryan.lichangrui@gmail.com>
2021-08-21 21:42:37 -0400

    add ingredient ui (no backend implementation yet)

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -8,6 +8,9 @@ import {
     Input,
     FormControl,
     FormLabel,
+    InputRightAddon,
+    InputGroup,
+    Select,
 } from "@chakra-ui/react";
 import firebase from "../firebase";
 
@@ -48,34 +51,68 @@ const Dashboard = () => {
             justifyContent="center"
             alignItems="center"
             my="2rem"
+            px="5vw"
         >
-            <Avatar
-                name=""
-                size="xl"
-                src={JSON.parse(localStorage.getItem("user")).photoURL}
-            />
-            <Heading mt="5px" mb="20px">
-                {user.displayName}
-            </Heading>
-            <Button
-                mt="20px"
-                onClick={() => {
-                    localStorage.clear();
-                    window.location.href = "/";
-                }}
-            >
-                Log Out
-            </Button>
-            <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+            <Grid templateColumns="2fr 1fr" gap={6}>
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent="center"
+                    alignItems="center"
+                >
+                    <Avatar
+                        name=""
+                        size="xl"
+                        src={JSON.parse(localStorage.getItem("user")).photoURL}
+                    />
+                    <Heading mt="5px" mb="20px">
+                        {user.displayName}
+                    </Heading>
+                    <Button
+                        mt="20px"
+                        onClick={() => {
+                            localStorage.clear();
+                            window.location.href = "/";
+                        }}
+                        colorScheme="red"
+                    >
+                        Log Out
+                    </Button>
+                </Box>
+                <Box
+                    display="flex"
+                    flexDirection="column"
+                    justifyContent="space-evenly"
+                >
+                    <FormLabel>Add Ingredient</FormLabel>
+                    <Input placeholder="e.g tomato" />
+                    <Select placeholder="Select Unit">
+                        <option value="mL">mL</option>
+                        <option value="L">L</option>
+                        <option value="lbs">lbs</option>
+                        <option value="pt">pt</option>
+                        <option value="tsp">tsp</option>
+                        <option value="C">C</option>
+                        <option value="mg">mg</option>
+                        <option value="g">g</option>
+                        <option value="Tbsp">Tbsp</option>
+                    </Select>
+                    <Button colorScheme="green">Add</Button>
+                </Box>
+            </Grid>
+            <Grid templateColumns="repeat(4, 1fr)" gap={6} my="50px">
                 {ingredients.map((ingredient) => (
                     <FormControl key={ingredient.name}>
                         <FormLabel>{ingredient.name}</FormLabel>
-                        <Input
-                            value={ingredient.amount}
-                            name={ingredient.name}
-                            type="number"
-                            onChange={handleChange}
-                        ></Input>
+                        <InputGroup>
+                            <Input
+                                value={ingredient.amount}
+                                name={ingredient.name}
+                                type="number"
+                                onChange={handleChange}
+                            ></Input>
+                            <InputRightAddon children={ingredient.unit} />
+                        </InputGroup>
                     </FormControl>
                 ))}
             </Grid>
